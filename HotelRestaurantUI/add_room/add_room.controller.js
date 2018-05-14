343659b958196721f0107f0e1004faeec86d6630
BENCE-PC\Bence <pelokbence@yahoo.com>
2018-05-14 22:39:40 +0300

    Fine-tuned Add room Options; Can now add additional Extras

--- HotelRestaurantUI/add_room/add_room.controller.js
+++ HotelRestaurantUI/add_room/add_room.controller.js
@@ -11,77 +11,40 @@
 
 		vm.check = check;
 		vm.newText = "";
+		vm.newExtra = "";
 		vm.dataLoading = false;
 
-		$scope.add_conf = add_conf;
-		$scope.add_room = add_room;
+		vm.add_conf = add_conf;
+		vm.add_extra = add_extra;
+		vm.add_room = add_room;
 
-		$scope.rTypes = [
-			{ val: 'bedroom', name: 'Bedroom' },
-			{ val: 'bathroom', name: 'Bathroom' },
-			{ val: 'kitchen', name: 'Kitchen' }
-		];
+		vm.rTypes = ['Bedroom' ,'Bathroom' ,'Kitchen'];
 
-		$scope.rSubtypes = [];
-		$scope.rSubtypes.bedroom = [
-			{ val: 'single', name: '1 Single Bed' },
-			{ val: 'double', name: '1 Double Bed' },
-			{ val: 'bunk', name: '1 Bunk Bed' },
-			{ val: 'dual-single', name: '2 Single Beds' },
-			{ val: 'family-lite', name: '1 Double Bed + 1 Single Bed' },
-			{ val: 'family', name: '1 Double Bed + 2 Single Beds' },
-			{ val: 'dual-double', name: '2 Double Beds' },
-			{ val: 'dual-bunk', name: '2 Bunk Beds' },
-		];
-		$scope.rSubtypes.bathroom = [
-			{ val: 'toilet', name: 'Toilet' },
-			{ val: 'shower', name: 'Shower Room' },
-		];
-		$scope.rSubtypes.kitchen = [
-			{ val: 'cookery', name: 'Cookery' },
-			{ val: 'diner', name: 'Diner' },
-		];
+		vm.rSubtypes = [];
+		vm.rSubtypes.Bedroom = ['1 Single Bed', '1 Double Bed', '1 Bunk Bed', '2 Single Beds', '1 Double Bed + 1 Single Bed', '1 Double Bed + 2 Single Beds', '2 Double Beds', '2 Bunk Beds'];
+		vm.rSubtypes.Bathroom = ['Toilet' ,'Shower Room'];
+		vm.rSubtypes.Kitchen = ['Cookery', 'Diner'];
 
-		$scope.rSizes = [
-			{ val: 'S', name: 'Small' },
-			{ val: 'M', name: 'Medium' },
-			{ val: 'L', name: 'Large' },
-			{ val: 'XL', name: 'Very Large' },
-			{ val: 'XXL', name: 'Extra Large' },
-		];
+		vm.rSizes = ['Small', 'Medium', 'Large', 'Very Large', 'Extra Large'];
 
-		$scope.rExtras = [];
-		$scope.rExtras.bedroom = [
-			{ val: 'minifridge', name: 'Mini Fridge' },
-			{ val: 'tv', name: 'Television' },
-			{ val: 'ac', name: 'Air Conditioning' },
-			{ val: 'balcony', name: 'Balcony' },
-			{ val: 'wardrobe', name: 'Wardrobe' },
-			{ val: 'coffee', name: 'Coffee Machine' }
-		];
-		$scope.rExtras.bathroom = [
-			{ val: 'toilet', name: 'Toilet' },
-			{ val: 'shower', name: 'Shower Cabin' },
-			{ val: 'sink', name: 'Sink' },
-			{ val: 'tub', name: 'Bathtub' },
-			{ val: 'bidet', name: 'Bidet' }
-		];
-		$scope.rExtras.kitchen = [
-			{ val: 'oven', name: 'Oven' },
-			{ val: 'microwave', name: 'Microwave' },
-			{ val: 'sink', name: 'Sink' },
-			{ val: 'fridge', name: 'Refrigerator' }
-		];
+		vm.rExtras = [];
+		vm.rExtras.Bedroom = ['Mini Fridge', 'Television', 'Air Conditioning', 'Balcony', 'Wardrobe', 'Coffee Machine'];
+		vm.rExtras.Bathroom = ['Toilet', 'Shower Cabin', 'Sink', 'Bathtub', 'Bidet'];
+		vm.rExtras.Kitchen = ['Oven', 'Microwave', 'Sink', 'Refrigerator'];
 
 		function check() {
 			FlashService.Error('Well... this is awkward...');
 		};
 
 		function add_conf(subtype) {
-			var tboxval = document.getElementById("newconfval");
 			var tboxname = document.getElementById("newconfname");
-			subtype.push({ val: tboxval.value, name: tboxname.value });
-			tboxval.value = "";
+			subtype.push(tboxname.value);
+			tboxname.value = "";
+		}
+
+		function add_extra(extra) {
+			var tboxname = document.getElementById("newextra");
+			extra.push(tboxname.value);
 			tboxname.value = "";
 		}
 
