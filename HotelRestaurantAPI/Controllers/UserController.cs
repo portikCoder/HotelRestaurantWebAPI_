4bee1200a3ae04b98e96a26b2621e3ca850642a9
portikCoder <33704943+portikCoder@users.noreply.github.com>
2018-05-17 00:43:43 +0300

    Added test routes for [Preservations] On admin site & user site as well(here double route).

--- HotelRestaurantAPI/Controllers/UserController.cs
+++ HotelRestaurantAPI/Controllers/UserController.cs
@@ -23,20 +23,54 @@ namespace HotelRestaurantAPI.Controllers
             List<Room> rooms = new List<Room>();
 
             /*test data*/
-            List<(string Id, string Type, string Subtype, string[] Properties, string[] Others)> rooms_t = new List<(string Id, string Type, string Subtype, string[] Properties, string[] Others)>(){
-                ("room_404", "bedroom", "doubled penetratin", new string[] { "extra-large", "XXL"}, new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" }),
-                ("room_406", "bedroom", "doubled penetratin", new string[] { "extra-large", "XXL"}, new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" }),
-                ("room_408", "bedroom", "doubled penetratin", new string[] { "extra-large", "XXL"}, new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" }),
-
-            };
             /*test data*/
 
-            //return Ok(new[] { "room_404", "bedroom", "doubled penetratin", new string[] { "extra-large", "XXL" }, new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" } }.ToList());
-            //return Ok( new { "data" = 1, "data2" = 2, "data3" = 3 } );
             return Ok(new[] { 
                 new { Id = "room_404", Type = "bedroom", Subtype = "doubled penetratin", Properties = new string[] { "extra-large", "XXL" }, Others = new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" }},
                 new { Id = "room_406", Type = "bedroom", Subtype = "doubled penetratin", Properties = new string[] { "extra-large", "XXL" }, Others = new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" }},
                 new { Id = "room_408", Type = "bedroom", Subtype = "doubled penetratin", Properties = new string[] { "extra-large", "XXL" }, Others = new string[] { "mini-skirt", "ammm wardrobe", "mini refrigerator" }},
+            });
+        }
+
+        [HttpPost]
+        [Route("api2/userpreservations")]
+        public IHttpActionResult GetPreservations(UserDTO user)
+        {
+            /* Get tthe lists of rooms from DB!!!! */
+            List<Room> rooms = new List<Room>();
+
+            /*test data*/
+            /*test data*/
+
+            /*
+             * 
+             "Approved" == 1
+             "Pending" == 0
+             */
+
+            return Ok(new[] {
+                new { Id = "room_404", StartDate = DateTime.Now.AddDays(0), EndDate = DateTime.Now.AddDays(2), Status = 1, Price=(1000)},
+                new { Id = "room_406", StartDate = DateTime.Today.AddDays(4), EndDate = DateTime.Today.AddDays(7), Status = 0, Price=(5500)},
+                new { Id = "room_408", StartDate = DateTime.Today.AddDays(5), EndDate = DateTime.Today.AddDays(6), Status = 0, Price=(750)},
+            });
+        }
+
+        [HttpPost]
+        [Route("api2/otheruserpreservations")]
+        public IHttpActionResult GetOtherPreservations(UserDTO user)
+        {
+            /* Get tthe lists of rooms from DB!!!! */
+            List<Room> rooms = new List<Room>();
+
+            /*test data*/
+            /*test data*/
+
+            return Ok(new[] {
+                new { Id = "room_404", StartDate = DateTime.Now.AddDays(2), EndDate = DateTime.Now.AddDays(4), },
+                //new { Id = "room_406", StartDate = DateTime.Today.AddDays(8), EndDate = DateTime.Today.AddDays(9), },
+                new { Id = "room_406", StartDate = DateTime.Today.AddDays(8), EndDate = DateTime.Today.AddDays(9), },
+                new { Id = "room_408", StartDate = DateTime.Today.AddDays(8), EndDate = DateTime.Today.AddDays(10), },
+                new { Id = "room_409", StartDate = DateTime.Today.AddDays(2), EndDate = DateTime.Today.AddDays(10), },
             });
         }
     }
